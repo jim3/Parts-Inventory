0a08141a353dde40b87f0d52e4f8abe64db50c54
jim3 <j3.casey@protonmail.com>
2023-05-08 22:39:46 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-
 // Part interface
 interface Part {
     partCategory: string;
@@ -52,4 +51,3 @@ function insertIntoDOM(parts: PartType[]): void {
 
 // Insert the output into the DOM
 insertIntoDOM(parts);
-
